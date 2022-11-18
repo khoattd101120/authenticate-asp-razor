1d634e04191f7fcba7049f6aa3ddf9fe068d136e
khoattd1011 <cc10112000@gmail.com>
2022-11-18 23:13:08 +0700

    Init Project

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using authenticate_asp_razor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace authenticate_asp_razor.Data
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<ApplicationUser>? Users { get; set; }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace authenticate_asp_razor.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set;}
+    }
+}
